5bf9f5ade4745ecf780cf072c000be09f0756eaa
Tikam <tikam@Tikams-Mac-mini.local>
2026-06-04 13:22:06 +0530

    fix: add cornical tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,95 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         gtag('js', new Date());
         gtag('config', 'G-H286ZDP071');`}
         </script>
-        
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@graph": [
+                {
+                  "@type": "Organization",
+                  "@id": "https://skinwin.co.in/#organization",
+                  "name": "SkinWin",
+                  "url": "https://skinwin.co.in",
+                  "logo": "https://skinwin.co.in/logo.png",
+                  "email": "info@skinwin.co.in",
+                  "telephone": "+91 97733 11108",
+                  "sameAs": [
+                    "https://www.instagram.com/skinwinclinicofficial/",
+                    "https://www.facebook.com/skinwinclinicofficial/"
+                  ]
+                },
+                {
+                  "@type": "MedicalClinic",
+                  "@id": "https://skinwin.co.in/#medicalclinic",
+                  "name": "SkinWin",
+                  "url": "https://skinwin.co.in",
+                  "image": "https://skinwin.co.in/logo.png",
+                  "telephone": "+91 97733 11108",
+                  "email": "info@skinwin.co.in",
+                  "priceRange": "₹₹",
+                  "medicalSpecialty": [
+                    "Dermatology",
+                    "Hair Restoration",
+                    "Cosmetic Dermatology"
+                  ],
+                  "founder": {
+                    "@type": "Person",
+                    "name": "Dr. Vijay Gakhar"
+                  },
+                  "address": {
+                    "@type": "PostalAddress",
+                    "streetAddress": "2nd Floor, Vijay ENT Hospital, St. Stephen Circle, Makarwali Rd, Cheeta Nagar, Abhiyanta Nagar",
+                    "addressLocality": "Ajmer",
+                    "addressRegion": "Rajasthan",
+                    "postalCode": "305004",
+                    "addressCountry": "IN"
+                  },
+                  "hasMap": "https://maps.app.goo.gl/AN9d55ahA1QgZLML8",
+                  "openingHoursSpecification": [
+                    {
+                      "@type": "OpeningHoursSpecification",
+                      "dayOfWeek": [
+                        "Monday",
+                        "Tuesday",
+                        "Wednesday",
+                        "Thursday",
+                        "Friday",
+                        "Saturday"
+                      ],
+                      "opens": "10:00",
+                      "closes": "20:00"
+                    },
+                    {
+                      "@type": "OpeningHoursSpecification",
+                      "dayOfWeek": "Sunday",
+                      "opens": "10:00",
+                      "closes": "19:00"
+                    }
+                  ],
+                  "aggregateRating": {
+                    "@type": "AggregateRating",
+                    "ratingValue": "4.8",
+                    "reviewCount": "194",
+                    "bestRating": "5",
+                    "worstRating": "1"
+                  }
+                },
+                {
+                  "@type": "WebSite",
+                  "@id": "https://skinwin.co.in/#website",
+                  "url": "https://skinwin.co.in",
+                  "name": "SkinWin",
+                  "publisher": {
+                    "@id": "https://skinwin.co.in/#organization"
+                  },
+                  "inLanguage": "en-IN"
+                }
+              ]
+            }),
+          }}
+        />
       </head>
       <body className={`${montserrat.variable} ${cormorant.variable} h-full`}>
         <PageLayout>
